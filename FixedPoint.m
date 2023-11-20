46c297870e35c2e9833d6e02fe0b206ea1ab938e
Gursamrath Singh R Kohli <126102220+gursamrathSingh@users.noreply.github.com>
2023-11-20 10:37:27 +0530

    Roots of Non-Linear Equations

--- FixedPoint.m
+++ FixedPoint.m
@@ -1,5 +1,4 @@
 %Fixed point iterative method to find the fixed point of the given eqn.
-%{
 clc, clearvars
 g = @(x) x - (x^3 + 4*x^2 - 10)/(3*x^2 + 8);
 x0 = 0; x1 = g(x0);
@@ -10,14 +9,5 @@ while abs(x0-x1) > 0.001
     i = i+1;
 end
 x1,i
-%}
-clc, clearvars
-g = @(x) x - (x^3 + 4*x^2 - 10)/(3*x^2 + 8);
-x0=0; x1=g(x0);
-while abs(x0-x1)>0.001
-    x0=x1;
-    x1=g(x0);
-end
-fprintf("%f",x1);
 
 
