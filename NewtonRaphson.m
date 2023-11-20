3d68b70087349fd80c2349a6cdd178cae0e8424b
Gursamrath Singh R Kohli <126102220+gursamrathSingh@users.noreply.github.com>
2023-11-20 10:38:03 +0530

    Roots of Non-Linear Equations

--- NewtonRaphson.m
+++ NewtonRaphson.m
@@ -1,8 +1,6 @@
 %Newton Raphson method to find roots of a qiven eqn
-%{
 clc, clearvars
 f = @(x) cos(x);
-
 syms x;
 g = diff(f,x);
 p0=1 ;
@@ -12,17 +10,3 @@ while abs(p0-p1)>0.001
     p1=p0 - f(p0)/subs(g,p0);
 end
 fprintf('%f',p1);
-%}
-clc, clearvars
-f = @(x) cos(x) - x^2;
-syms x
-h = diff(f,x);
-p0 = 0.8;
-p1 = p0 - f(p0)/subs(h,p0);
-itr = 0;
-while abs(p0-p1) > 0.001
-    p0 = p1;
-    p1 = p0 - f(p0)/subs(h,p0);
-    itr = itr + 1; 
-end
-fprintf("%f\n%d",p1, itr);
